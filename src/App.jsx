6c74af111406c57ecd9034c58972fa66de2628a7
naokiteshima <63271045+naokiteshima@users.noreply.github.com>
2022-07-14 09:54:09 +0900

    aaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [onoffFlag, setOnoffFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickCountDown = () => {
+    setNum(num - 1);
+  };
+
+  const changeOnoffFlag = () => {
+    setOnoffFlag(!onoffFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      onoffFlag || setOnoffFlag(true);
+    } else {
+      onoffFlag && setOnoffFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気</ColorfulMessage>
       <ColorfulMessage color="pink">お元気！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickCountDown}>カウントダウン</button>
+      <button onClick={changeOnoffFlag}>on/off</button>
       <p>{num}</p>
+      {onoffFlag && <p>fdhisufhaidfhsod</p>}
     </>
   );
 };
